7e28f397b23ed60ab45c2e5286e32cd1f50c51da
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-08-16 02:17:57 +0200

    Add examples

--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from '../components'
 
-ReactDOM.render(<App/>, document.getElementById('content'));
+import {
+  SelectField,
+  MultiSelectField,
+  Option
+} from '../components';
+
+ReactDOM.render(
+  <div>
+    <SelectField label={'Default'}>
+      <Option value={1}>One</Option>
+      <Option value={2}>Two</Option>
+      <Option value={3}>Three</Option>
+      <Option value={4}>Four</Option>
+      <Option value={5}>Five</Option>
+    </SelectField>
+    <SelectField label={'Editable'} editable>
+      <Option value={1}>One</Option>
+      <Option value={2}>Two</Option>
+      <Option value={3}>Three</Option>
+      <Option value={4}>Four</Option>
+      <Option value={5}>Five</Option>
+    </SelectField>
+    <MultiSelectField label={'Multiselect'}>
+      <Option value={1}>Value One</Option>
+      <Option value={2}>Value Two</Option>
+      <Option value={3}>Value Three</Option>
+      <Option value={4}>Value Four</Option>
+      <Option value={5}>Value Five</Option>
+    </MultiSelectField>
+  </div>,
+  document.getElementById('content'));
